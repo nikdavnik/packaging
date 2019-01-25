6014a5c4a6727f8af1580ab858979b417c40fcee
Adrian Alves <aalves@gmail.com>
2019-01-25 12:25:39 -0500

    xenial build files

---
+++ deb/xenial/fetch-sources.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+VER=$1
+INSTALL_VER=$2
+
+DIRWEB="gluu-server.amd64/gluu-server-3.1.5/opt/dist/gluu"
+COMMUNITY="gluu-server.amd64/gluu-server-3.1.5/install"
+
+INSTALL="master"
+if [ -n "${INSTALL_VER}" ]; then
+    INSTALL=$INSTALL_VER
+fi
+
+if [ -n "${VER}" ]; then
+    wget -nv http://ox.gluu.org/maven/org/xdi/oxshibbolethIdp/$VER/oxshibbolethIdp-$VER.war -O $DIRWEB/idp.war
+    wget -nv http://ox.gluu.org/maven/org/xdi/oxtrust-server/$VER/oxtrust-server-$VER.war -O $DIRWEB/identity.war
+    wget -nv http://ox.gluu.org/maven/org/xdi/oxauth-server/$VER/oxauth-server-$VER.war -O $DIRWEB/oxauth.war
+    #wget -nv http://ox.gluu.org/maven/org/xdi/oxauth-rp/$VER/oxauth-rp-$VER.war -O $DIRWEB/oxauth-rp.war
+    wget -nv http://ox.gluu.org/maven/org/xdi/oxShibbolethStatic/$VER/oxShibbolethStatic-$VER.jar -O $DIRWEB/shibboleth-idp.jar
+    wget -nv http://ox.gluu.org/maven/org/xdi/oxShibbolethKeyGenerator/$VER/oxShibbolethKeyGenerator-$VER.jar -O $DIRWEB/idp3_cml_keygenerator.jar
+    rm -rf $COMMUNITY/community-edition-setup*
+    curl -LkSs https://codeload.github.com/GluuFederation/community-edition-setup/zip/$INSTALL -o $COMMUNITY/community-edition-setup.zip
+    unzip $COMMUNITY/community-edition-setup.zip -d $COMMUNITY
+    mv -nv $COMMUNITY/community-edition-setup-$INSTALL $COMMUNITY/community-edition-setup
+    rm -rf $COMMUNITY/community-edition-setup.zip
+    wget https://ox.gluu.org/npm/passport/passport-3.1.5.tgz -O $DIRWEB/passport.tgz
+    wget https://ox.gluu.org/npm/passport/passport-version_3.1.5-node_modules.tar.gz -O $DIRWEB/passport-version_3.1.5-node_modules.tar.gz
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/initd/gluu-server -O gluu-server.amd64/debian/gluu-server-3.1.5.init.d
+    chmod +x gluu-server.amd64/debian/gluu-server-3.1.5.init.d
+fi
