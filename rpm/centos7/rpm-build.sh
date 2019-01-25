c4a348142444dac9744399085d1ab6e32c063f15
Adrian Alves <adrian@gluu.org>
2019-01-25 13:02:11 -0500

    centos7 files for 4.0

---
+++ rpm/centos7/rpm-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define pathes
+current_dir=`pwd`
+gluu_ce_path="$current_dir"
+rpmbuild_path="$current_dir/rpmbuild"
+
+# Prepare build folders
+mkdir -p $rpmbuild_path/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+
+# Spec file name
+specfile=gluu-CE.spec
+
+# Prepare sources
+cd $gluu_ce_path
+
+/bin/tar czvf gluu-server-4.0.tar.gz --exclude=".gitignore" gluu-server-4.0
+/bin/mv gluu-server-4.0.tar.gz $rpmbuild_path/SOURCES/
+/bin/cp gluu-serverd-4.0 $rpmbuild_path/SOURCES/gluu-serverd-4.0
+/bin/cp systemd-unitfile $rpmbuild_path/SOURCES/systemd-unitfile
+/bin/cp profile $rpmbuild_path/SOURCES/
+/bin/cp $specfile $rpmbuild_path/SPECS/
+
+# Run build
+rpmbuild -ba --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
