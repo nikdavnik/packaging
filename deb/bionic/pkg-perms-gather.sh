172096e3f36fc9cc77bae013bc1ca32a5d047afb
root <root@pkg-deb3.gluu.org>
2019-01-25 18:30:00 +0000

    bionic files

---
+++ deb/bionic/pkg-perms-gather.sh
@@ -0,0 +1,32 @@
+Format: 1.8
+Date: Thu, 29 Oct 2015 14:21:54 +0200
+Source: gluu-server-3.1.4
+Binary: gluu-server-3.1.4
+Architecture: source amd64
+Version: 1.RC6~bionic+Ub18.04
+Distribution: unstable
+Urgency: medium
+Maintainer: Gluu Developers <support@gluu.org>
+Changed-By: Adrian Alves <support@gluu.org>
+Description:
+ gluu-server-3.1.4 - Gluu Server Community Edition
+Changes:
+ gluu-server-3.1.4 (1.RC6~bionic+Ub18.04) unstable; urgency=medium
+ .
+   * New version 3.1.4
+Checksums-Sha1:
+ eba2dc1fcf13b785ad88e5f6b5ad32682c9167ef 718 gluu-server-3.1.4_1.RC6~bionic+Ub18.04.dsc
+ f5ac34c0e7c2f7989bca40da08bded51288dd4ee 891300411 gluu-server-3.1.4_1.RC6~bionic+Ub18.04.tar.gz
+ f3ac2eca7fec76f2fe46ef0801238cd2644d3c00 5544 gluu-server-3.1.4_1.RC6~bionic+Ub18.04_amd64.buildinfo
+ e42db3a520f1c8a37fa534879d64b6fe62e11e60 784555772 gluu-server-3.1.4_1.RC6~bionic+Ub18.04_amd64.deb
+Checksums-Sha256:
+ 6304cf63b98567341a946c1f1db6c775b646ca58beff056037902508234e2e03 718 gluu-server-3.1.4_1.RC6~bionic+Ub18.04.dsc
+ 4a3c511ead977807b22671d9dceb9c4c42b25af18792f6b72d63831fbed1f6c8 891300411 gluu-server-3.1.4_1.RC6~bionic+Ub18.04.tar.gz
+ fec4bdb835651baa112cc8d76c4b37b30982aaf1b711cee238aef6deb988929c 5544 gluu-server-3.1.4_1.RC6~bionic+Ub18.04_amd64.buildinfo
+ 78c7c0750454cd154d33ac19167b951f68dadda5db6afec51256e52585c6cf0e 784555772 gluu-server-3.1.4_1.RC6~bionic+Ub18.04_amd64.deb
+Files:
+ 5714486a5e940ee109fbc4545a5e3b95 718 java optional gluu-server-3.1.4_1.RC6~bionic+Ub18.04.dsc
+ 34f12885d8576ead3c1e0f16af275cfd 891300411 java optional gluu-server-3.1.4_1.RC6~bionic+Ub18.04.tar.gz
+ 5fa71aa17a21e5ce61b9d3998e2af33a 5544 java optional gluu-server-3.1.4_1.RC6~bionic+Ub18.04_amd64.buildinfo
+ 8963bfa2aa4c0dbdf96102126b19c776 784555772 java optional gluu-server-3.1.4_1.RC6~bionic+Ub18.04_amd64.deb
+Original-Maintainer: Adrian Alves <support@gluu.org>
