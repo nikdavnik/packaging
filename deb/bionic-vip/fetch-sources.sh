2fd0f2db00107e259230949217c584d01251f288
David <nikdavnik@gmail.com>
2019-04-30 12:45:23 +0400

    Update fetch-sources.sh

--- deb/bionic-vip/fetch-sources.sh
+++ deb/bionic-vip/fetch-sources.sh
@@ -32,4 +32,11 @@ if [ -n "${VER}" ]; then
    
     mkdir -p gluu-server.amd64/gluu-server-vip-4.0/opt/couchbase/
     wget https://packages.couchbase.com/releases/6.0.1/couchbase-server-enterprise_6.0.1-ubuntu18.04_amd64.deb -O gluu-server.amd64/gluu-server-vip-4.0/opt/couchbase/couchbase-server-enterprise_6.0.1-ubuntu18.04_amd64.deb
+    
+    # systemd files for services
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/identity.service -O gluu-server.amd64/gluu-server-4.0/lib/systemd/system/identity.service 
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/opendj.service -O gluu-server.amd64/gluu-server-4.0/lib/systemd/system/opendj.service
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/oxauth-rp.service -O gluu-server.amd64/gluu-server-4.0/lib/systemd/system/oxauth-rp.service
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/oxauth.service -O gluu-server.amd64/gluu-server-4.0/lib/systemd/system/oxauth.service
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/passport.service -O gluu-server.amd64/gluu-server-4.0/lib/systemd/system/passport.service
 fi
