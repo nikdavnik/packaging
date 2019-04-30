cbc65dae7a43cd03c2ba95a7026205b74ae40da3
David <nikdavnik@gmail.com>
2019-04-30 20:06:36 +0400

    Update fetch-sources.sh

--- rpm/rhel7-vip/fetch-sources.sh
+++ rpm/rhel7-vip/fetch-sources.sh
@@ -36,4 +36,12 @@ if [ -n "${VER}" ]; then
 
     mkdir -p gluu-server-vip-4.0/opt/couchbase/
     wget https://packages.couchbase.com/releases/6.0.1/couchbase-server-enterprise-6.0.1-centos7.x86_64.rpm -O gluu-server-vip-4.0/opt/couchbase/couchbase-server-enterprise-6.0.1-centos7.x86_64.rpm
+    
+    # systemd files for services
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/identity.service -O gluu-server-4.0/lib/systemd/system/identity.service 
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/opendj.service -O gluu-server-4.0/lib/systemd/system/opendj.service
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/oxauth-rp.service -O gluu-server-4.0/lib/systemd/system/oxauth-rp.service
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/oxauth.service -O gluu-server-4.0/lib/systemd/system/oxauth.service
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/systemd/passport.service -O gluu-server-4.0/lib/systemd/system/passport.service
+    
 fi
