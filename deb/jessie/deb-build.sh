e8dcea5dc93a654757ef32d69f2b18dd2307204a
David <nikdavnik@gmail.com>
2019-06-21 00:47:13 +0400

    Update deb-build.sh

--- deb/jessie/deb-build.sh
+++ deb/jessie/deb-build.sh
@@ -7,8 +7,8 @@ if [ "$1" != "" ]; then
     build_root=$1
 fi
 
-chmod 544 "${build_root}/gluu-server-4.0/tmp/system_user.list"
-chmod 544 "${build_root}/gluu-server-4.0/tmp/system_group.list"
+chmod 544 "${build_root}/gluu-server/tmp/system_user.list"
+chmod 544 "${build_root}/gluu-server/tmp/system_group.list"
 
 # Run Build
 pushd gluu-server.amd64
