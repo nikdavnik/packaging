47677f29b975ba1218d210234e728bad9bc9ea10
David <nikdavnik@gmail.com>
2019-08-19 18:36:39 +0400

    Update fetch-sources.sh

--- deb/xenial/fetch-sources.sh
+++ deb/xenial/fetch-sources.sh
@@ -25,8 +25,8 @@ if [ -n "${VER}" ]; then
     rm -rf $COMMUNITY/community-edition-setup.zip
     wget https://ox.gluu.org/npm/passport/passport-4.0.0.tgz -O $DIRWEB/passport.tgz
     wget https://ox.gluu.org/npm/passport/passport-$INSTALL-node_modules.tar.gz -O $DIRWEB/passport-$INSTALL-node_modules.tar.gz
-    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/initd/gluu-server -O gluu-server.amd64/debian/gluu-server-4.0.init.d
-    chmod +x gluu-server.amd64/debian/gluu-server-4.0.init.d    
+    wget https://raw.githubusercontent.com/GluuFederation/community-edition-package/$INSTALL/package/initd/gluu-server -O gluu-server.amd64/debian/gluu-server.init.d
+    chmod +x gluu-server.amd64/debian/gluu-server.init.d    
     wget -nv https://ox.gluu.org/maven/org/gluu/super-gluu-radius-server/4.0.0-SNAPSHOT/super-gluu-radius-server-4.0.0-SNAPSHOT-distribution.zip -O $DIRWEB/gluu-radius-libs.zip
     wget -nv https://ox.gluu.org/maven/org/gluu/super-gluu-radius-server/4.0.0-SNAPSHOT/super-gluu-radius-server-4.0.0-SNAPSHOT.jar -O $DIRWEB/super-gluu-radius-server.jar
 fi
