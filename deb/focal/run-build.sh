e4b1e89f5b88e7c19a8fc734c6b358a6654ce665
David <nikdavnik@gmail.com>
2022-02-16 16:22:03 +0400

    Update run-build.sh

--- deb/focal/run-build.sh
+++ deb/focal/run-build.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-#rm -rf *.gz *.deb *.build* *.changes *.dsc
-#rm -rf jans-1.0.0/*
-#mkdir -p jans-1.0.0
-#cp install.py jans-1.0.0/install.py
-#cd jans-1.0.0
-
-
-#python3 install.py --no-setup
-rm -rf install.py
-rm -rf tmp
-rm -rf install
-rm -rf jans-cli
-
-touch opt/jans/jans-setup/package
-
-tar cvfz ../jans_1.0.0.tar.gz *
-cp -a ../debian .
-tar cvfz ../jans_1.0.0.orig.tar.gz *
+tar cvfz jans_1.0.0.tar.gz jans-1.0.0/*
+cp -a debian jans-1.0.0/
+tar cvfz jans_1.0.0.orig.tar.gz jans-1.0.0/*
 debuild -us -uc
 cd ..
