83008365689452d23b21f310d9d6ecb8fcc60a93
David <nikdavnik@gmail.com>
2022-02-17 12:30:36 +0400

    Update run-build.sh

--- deb/focal/run-build.sh
+++ deb/focal/run-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-tar cvfz jans_1.0.0.tar.gz jans-1.0.0/*
-cp -a debian jans-1.0.0/
-tar cvfz jans_1.0.0.orig.tar.gz jans-1.0.0/*
+cd jans-1.0.0
+tar cvfz ../jans_1.0.0.tar.gz *
+cp -a ../debian .
+tar cvfz ../jans_1.0.0.orig.tar.gz *
 debuild -us -uc
 cd ..
