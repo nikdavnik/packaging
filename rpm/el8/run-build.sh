b1122605c9f35666e23228c132b3d8079eec848f
nikdavnik <nikdavnik@gmail.com>
2022-02-17 20:32:24 +0400

    updated

--- rpm/el8/run-build.sh
+++ rpm/el8/run-build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+cd jans-%VERSION%
 
-status=`pgrep rpm-build`
-if [ "$status" != "" ]; then
-    echo "process is running"
-    exit 1
-fi
+current_dir=`pwd`
+rpmbuild_path="$current_dir/rpmbuild"
+mkdir -p $rpmbuild_path/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+specfile=jans.spec
+cp $current_dir/$specfile $rpmbuild_path/SPECS/.
+tar cvfz jans-%VERSION%.tar.gz jans-%VERSION%
+cp jans-%VERSION%.tar.gz $rpmbuild_path/SOURCES/.
+rm -rf rpmbuild/RPMS/x86_64/*
+rpmbuild -bb --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
 
-./rpm-build.sh
