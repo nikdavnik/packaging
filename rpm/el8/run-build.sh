e81afd6c218a3bc5b60a892013d8c77ee8d9e850
nikdavnik <nikdavnik@gmail.com>
2022-02-18 12:55:02 +0400

    updated

--- rpm/el8/run-build.sh
+++ rpm/el8/run-build.sh
@@ -6,6 +6,7 @@ current_dir=`pwd`
 echo "V: $VERSION R: $REL"
 sed -i "s/%VERSION%/$VERSION/g" jans.spec
 sed -i "s/%REL%/$REL/g" jans.spec
+cat jans.spec
 rpmbuild_path="$current_dir/rpmbuild"
 mkdir -p $rpmbuild_path/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 specfile=jans.spec
