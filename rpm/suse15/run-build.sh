546f9b689942856007a6192b9cd6b53147290955
nikdavnik <nikdavnik@gmail.com>
2022-02-18 16:19:17 +0400

    updated

--- rpm/suse15/run-build.sh
+++ rpm/suse15/run-build.sh
@@ -3,16 +3,15 @@
 VERSION=$(echo "%VERSION%" | awk -F '-' {'print $1'})
 REL=$(echo "%VERSION%" | cut -d'-' -f 2-)
 current_dir=`pwd`
-sed -i "s/%VER%/$VERSION/g" *.spec
-sed -i "s/%REL%/$REL/g" *.spec
+sed -i "s/%VER%/$VERSION/g" jans.spec
+sed -i "s/%REL%/$REL/g" jans.spec
 rpmbuild_path="$current_dir/rpmbuild"
 mkdir -p $rpmbuild_path/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-cp $current_dir/*.spec $rpmbuild_path/SPECS/.
+specfile=jans.spec
+cp $current_dir/$specfile $rpmbuild_path/SPECS/.
 mv jans-src jans-$VERSION
 tar cvfz jans-$VERSION.tar.gz jans-$VERSION
 cp jans-$VERSION.tar.gz $rpmbuild_path/SOURCES/.
+rm -rf rpmbuild/RPMS/x86_64/*
+rpmbuild -bb --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
 
-specfile=jans-el8.spec
-rpmbuild -bb --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
-specfile=jans-suse15.spec
-rpmbuild -bb --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
