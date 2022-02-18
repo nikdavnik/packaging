a1e274e245e2be585708bca1aedd10bd18acf453
nikdavnik <nikdavnik@gmail.com>
2022-02-18 12:22:38 +0400

    updated

--- rpm/el8/run-build.sh
+++ rpm/el8/run-build.sh
@@ -5,7 +5,8 @@ rpmbuild_path="$current_dir/rpmbuild"
 mkdir -p $rpmbuild_path/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 specfile=jans.spec
 cp $current_dir/$specfile $rpmbuild_path/SPECS/.
-tar cvfz jans-%VERSION%.tar.gz jans-src
+mv jans-src jans-%VERSION%
+tar cvfz jans-%VERSION%.tar.gz jans-%VERSION%
 cp jans-%VERSION%.tar.gz $rpmbuild_path/SOURCES/.
 rm -rf rpmbuild/RPMS/x86_64/*
 rpmbuild -bb --define "_topdir $rpmbuild_path" $rpmbuild_path/SPECS/$specfile
