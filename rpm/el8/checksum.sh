fc8ec66c1b3c9f7a45ce453cb13fb2a2966ce308
nikdavnik <nikdavnik@gmail.com>
2022-06-04 00:44:29 +0400

    ci: updated package name

--- rpm/el8/checksum.sh
+++ rpm/el8/checksum.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 VERSION=$(echo "%VERSION%" | awk -F '-' {'print $1'})
-REL=$(echo "%VERSION%" | cut -d'-' -f 2-)
-sha256sum rpmbuild/RPMS/x86_64/jans-$VERSION-$REL.el8.x86_64.rpm > rpmbuild/RPMS/x86_64/jans-$VERSION-$REL.el8.x86_64.rpm.sha256sum
+REL=$(echo "%VERSION%" | sed "s/^${VERSION}//g" | sed "s/^-//g")
+if [ -z "$REL" ]; then
+        RELEASE="el8"
+else
+        RELEASE="$REL.el8"
+fi
+sha256sum rpmbuild/RPMS/x86_64/jans-$VERSION-$RELEASE.x86_64.rpm > rpmbuild/RPMS/x86_64/jans-$VERSION-$RELEASE.x86_64.rpm.sha256sum
--- rpm/suse15/checksum.sh
+++ rpm/suse15/checksum.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 VERSION=$(echo "%VERSION%" | awk -F '-' {'print $1'})
-REL=$(echo "%VERSION%" | cut -d'-' -f 2-)
-sha256sum rpmbuild/RPMS/x86_64/jans-$VERSION-$REL.suse15.x86_64.rpm > rpmbuild/RPMS/x86_64/jans-$VERSION-$REL.suse15.x86_64.rpm.sha256sum
+REL=$(echo "%VERSION%" | sed "s/^${VERSION}//g" | sed "s/^-//g")
+if [ -z "$REL" ]; then
+        RELEASE="suse15"
+else
+        RELEASE="$REL.suse15"
+fi
+sha256sum rpmbuild/RPMS/x86_64/jans-$VERSION-$RELEASE.x86_64.rpm > rpmbuild/RPMS/x86_64/jans-$VERSION-$RELEASE.x86_64.rpm.sha256sum
